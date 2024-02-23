3694fe85b83f6e04ed3ed580e6da769077b5c6ed
richard <richardvalles94@gmail.com>
2024-02-23 13:04:35 -0600

    changes_recursividad

--- arch_examen.py
+++ arch_examen.py
@@ -1,2 +1,11 @@
 ##este ejemplo es para hacer el examen 
-print("hola  gente")
+def factorial (n):
+    if n == 0:
+        return 1 
+    else:
+        return n * factorial(n - 1)
+    
+# ejemplo de uso
+numero = 5
+resultado = factorial(numero)
+print(f"el factorial de {numero} es: {resultado}")
