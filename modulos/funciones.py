cd65b8b7a24bf8a68a9a56f8f6e3cfe1907b0293
richard <richardvalles94@gmail.com>
2024-02-23 15:06:58 -0600

    modulo

---
+++ modulos/funciones.py
@@ -0,0 +1,14 @@
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def divicion(a, b):
+    if b != 0:
+        return a / b 
+    else:
+        return "ERROR : divicion por cero"
---
+++ modulos/pricipal.py
@@ -0,0 +1,16 @@
+# contenido del archivo principal.py 
+
+import funciones
+
+#ejemplo de uso de las funciones del modulo 
+resultado_sume = funciones.suma(5, 3)
+print("Suma:", resultado_sume)
+
+resultado_resta = funciones.resta(10, 7)
+print("Suma:", resultado_resta)
+
+resultado_multiplicaion = funciones.multiplicacion(4, 6)
+print("Suma:", resultado_multiplicaion)
+
+resultado_divicion = funciones.divicion(8, 2)
+print("Suma:", resultado_divicion)
