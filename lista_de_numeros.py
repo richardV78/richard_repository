7e0255bfe6e3ecf56d4715b9e80d2ae04bbc89fe
richard <richardvalles94@gmail.com>
2024-02-23 13:46:52 -0600

    lista_de_numeros

---
+++ lista_de_numeros.py
@@ -0,0 +1,9 @@
+# lista de numero 
+numero = [1, 2, 3, 4, 5]
+
+# imprimir la lista original 
+print("Lista original: ", numero)
+
+# accer a elementos individuales por indice 
+print("Primer elemento: ", numero[0])
+print("Ultimo elemento: ", numero[-1])
