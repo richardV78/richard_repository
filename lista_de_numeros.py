cdd1ac18d369e00b1477def3aa027ce5de30c2dc
richard <richardvalles94@gmail.com>
2024-02-23 13:54:47 -0600

    changes_list

--- lista_de_numeros.py
+++ lista_de_numeros.py
@@ -7,3 +7,15 @@ print("Lista original: ", numero)
 # accer a elementos individuales por indice 
 print("Primer elemento: ", numero[0])
 print("Ultimo elemento: ", numero[-1])
+
+# reemplazar un elemento de la lista
+numero[2] = 10 
+print("lista despues de reemplazar el tercer elemento", numero)
+
+# agregar un elemento al fial de la lista 
+numero.append(6)
+print("lista despues de agregar un nuevo elemento",numero)
+
+# eliminar un elemneto de la lista por valor 
+numero.remove(4)
+print("lista despes de eliminar e numero 4", numero)
